a66b3a4a5fbf73530ac9b7eaea61853cec44ae18
Bukho Msengana <bukhomsengana@email.com>
2026-09-10 15:03:29 +0200

    Backup configs: 2026-09-10 15:03:29

---
+++ nvim/lua/plugins/colourschemes.lua
@@ -0,0 +1,10 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", lazy = true },
+  { "folke/tokyonight.nvim", lazy = true },
+  { "rebelot/kanagawa.nvim", lazy = true },
+  { "ellisonleao/gruvbox.nvim", lazy = true },
+  { "EdenEast/nightfox.nvim", lazy = true },
+  { "rose-pine/neovim", name = "rose-pine", lazy = true },
+  -- keep your current one too
+  { "Shatur/neovim-ayu", lazy = true },
+}
